053b5a13bbb7b3014f02617fcf90e34ac3a3f0b3
marmalademds <dave@massivedragonsoftware.com>
2025-02-04 12:36:04 +1100

    added styles and some src project structure

---
+++ src/styles/text.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export const text = StyleSheet.create({
+    fboldheader: {
+        fontSize: 30,
+        fontWeight: 'bold'
+    },
+    fboldsmall: {
+        fontSize: 14,
+        fontWeight: 'bold'
+    },
+    fsmall: {
+        fontSize: 14,
+    },
+    buttonText: {
+        color: 'white',
+    },
+    flinksmall: {
+        fontSize: 14,
+        color: 'blue',
+        fontWeight: 'bold',
+        textDecorationLine: 'underline'
+    }
+})
